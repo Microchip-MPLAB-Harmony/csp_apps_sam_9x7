9c16e4c81dfdbf85b685e6fb82cc63206d8b64a7
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-23 08:39:29 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_9x75_eb
    MH3-67021
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/packs/SAM9X75_DFP/instance/otpc.h
+++ apps/rtc/rtc_alarm/firmware/src/packs/SAM9X75_DFP/instance/otpc.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for ATSAM9X75D5M
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2021-12-02T16:30:36Z */
+/* file generated from device description version 2022-01-05T08:57:53Z */
 #ifndef _SAM9X7_OTPC_INSTANCE_
 #define _SAM9X7_OTPC_INSTANCE_
 
@@ -28,6 +28,6 @@
 /* ========== Instance Parameter definitions for OTPC peripheral ========== */
 #define OTPC_INSTANCE_ID                         _UL_(46)   /* OTP Controller */
 #define OTPC_EMULATION_ADDRESS                   _UL_(0x00400000) 
-#define OTPC_EMULATION_SIZE                      _UL_(0x00100000) 
+#define OTPC_EMULATION_SIZE                      _UL_(0x00001000) 
 
 #endif /* _SAM9X7_OTPC_INSTANCE_ */
