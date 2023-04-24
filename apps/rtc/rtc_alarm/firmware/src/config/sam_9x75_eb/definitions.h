3038af66c7d08a1b2b5fe156d957d6a4bd934441
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-04-24 11:58:04 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_9x75_eb
    MH3-78467

--- apps/rtc/rtc_alarm/firmware/src/config/sam_9x75_eb/definitions.h
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_9x75_eb/definitions.h
@@ -53,8 +53,8 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
-#include "peripheral/dbgu/plib_dbgu.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "peripheral/dbgu/plib_dbgu.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -63,6 +63,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "SAM9X75"
+#define DEVICE_ARCH			 "ARM926EJ-S"
+#define DEVICE_FAMILY		 "SAM9"
+#define DEVICE_SERIES		 "SAM9X7"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 800000000
