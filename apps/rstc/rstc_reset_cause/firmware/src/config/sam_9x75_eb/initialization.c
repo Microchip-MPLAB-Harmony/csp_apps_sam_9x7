8b8317b7af3eabc137b4c9a1bec3cf012f1bfb9e
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-04-24 11:58:03 +0530

    [RSTC] Regenerated demo for rstc_rstc_reset_cause_sam_9x75_eb
    MH3-78466

--- apps/rstc/rstc_reset_cause/firmware/src/config/sam_9x75_eb/initialization.c
+++ apps/rstc/rstc_reset_cause/firmware/src/config/sam_9x75_eb/initialization.c
@@ -139,6 +139,8 @@ static void SYSC_Disable( void )
  ********************************************************************************/
 static void STDIO_BufferModeSet(void)
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
 
     /* Make stdin unbuffered */
     setbuf(stdin, NULL);
@@ -162,6 +164,8 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
 	SYSC_Disable( );
 
@@ -191,6 +195,7 @@ void SYS_Initialize ( void* data )
 
 
 
+    /* MISRAC 2012 deviation block end */
 }
 
 
