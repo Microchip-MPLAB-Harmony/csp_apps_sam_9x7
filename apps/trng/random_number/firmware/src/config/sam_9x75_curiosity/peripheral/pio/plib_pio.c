f4a87676778b4bb96bafbf01d914e294f6f2e02c
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-28 17:35:24 +0530

    [TRNG] Regenerated demo for trng__random_number__firmware__sam_9x75_curiosity.X
    MH3-101220

--- apps/trng/random_number/firmware/src/config/sam_9x75_curiosity/peripheral/pio/plib_pio.c
+++ apps/trng/random_number/firmware/src/config/sam_9x75_curiosity/peripheral/pio/plib_pio.c
@@ -117,8 +117,8 @@ void PIO_Initialize ( void )
     /* PORTC Output Write Enable */
     ((pio_registers_t*)PIO_PORT_C)->PIO_OWER = PIO_OWER_Msk;
     /* PORTC Output Direction Enable */
-    ((pio_registers_t*)PIO_PORT_C)->PIO_OER = 0x0U;
-    ((pio_registers_t*)PIO_PORT_C)->PIO_ODR = ~0x0U;
+    ((pio_registers_t*)PIO_PORT_C)->PIO_OER = 0x4000U;
+    ((pio_registers_t*)PIO_PORT_C)->PIO_ODR = ~0x4000U;
     /* Initialize PORTC pin state */
     ((pio_registers_t*)PIO_PORT_C)->PIO_ODSR = 0x0U;
     /* PORTC Additional interrupt mode Enable */
